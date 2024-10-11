aebc6a721d11a6006cd42ff6be6766a2b6abe632
Kai Lüttmann <github@kai-luettmann.de>
2024-10-11 17:08:15 +0000

    commit from python

--- src/PublicApi/CatalogItemEndpoints/CatalogItemListPagedEndpoint.cs
+++ src/PublicApi/CatalogItemEndpoints/CatalogItemListPagedEndpoint.cs
@@ -39,7 +39,6 @@ public class CatalogItemListPagedEndpoint : IEndpoint<IResult, ListPagedCatalogI
 
     public async Task<IResult> HandleAsync(ListPagedCatalogItemRequest request, IRepository<CatalogItem> itemRepository)
     {
-        await Task.Delay(1000);
         var response = new ListPagedCatalogItemResponse(request.CorrelationId());
 
         var filterSpec = new CatalogFilterSpecification(request.CatalogBrandId, request.CatalogTypeId);
